2f0321757749ab274b2153aec098a8dc1a96e105
Bones <ben@bones.dev>
2025-11-27 12:10:48 +0000

    Header + navigation updates: Trustpilot badge component; currency selector refactored to shared select; JS supports select change; compact header tools spacing; search icon inside input; icon sizes to 2em; remove custom currency styles; widen search input; minor select partial enhancement with block options

--- assets/js/theme/global/currency-selector.js
+++ assets/js/theme/global/currency-selector.js
@@ -1,18 +1,28 @@
-import { showAlertModal } from './modal';
 import utils from '@bigcommerce/stencil-utils';
+import { showAlertModal } from './modal';
 
 let currencySelectorCalled = false;
 
 export default function (cartId) {
-    if (!cartId) return;
-
-    if (!currencySelectorCalled) {
-        currencySelectorCalled = true;
-    } else {
+    if (currencySelectorCalled) {
         return;
     }
 
-    function changeCurrency(url, currencyCode) {
+    currencySelectorCalled = true;
+
+    const hasCartId = Boolean(cartId);
+
+    const redirectTo = (url) => {
+        if (!url) return;
+        window.location.href = url;
+    };
+
+    function changeCurrency(url, currencyCode, fallbackUrl) {
+        if (!url || !currencyCode) {
+            redirectTo(fallbackUrl);
+            return;
+        }
+
         $.ajax({
             url,
             contentType: 'application/json',
@@ -21,38 +31,99 @@ export default function (cartId) {
         }).done(() => {
             window.location.reload();
         }).fail((e) => {
-            showAlertModal(JSON.parse(e.responseText).error);
-        });
-    }
-
-    $('[data-cart-currency-switch-url]').on('click', event => {
-        const currencySessionSwitcher = event.target.href;
-        event.preventDefault();
-        utils.api.cart.getCart({ cartId }, (err, response) => {
-            if (err || response === undefined) {
-                window.location.href = currencySessionSwitcher;
+            if (fallbackUrl) {
+                redirectTo(fallbackUrl);
                 return;
             }
 
-            const showWarning = response.discounts.some(discount => discount.discountedAmount > 0)
+            try {
+                const response = JSON.parse(e.responseText);
+                showAlertModal(response.error);
+            } catch (error) {
+                const fallbackMessage = (e && e.responseText) || (e && e.statusText) || '';
+                showAlertModal(fallbackMessage);
+            }
+        });
+    }
+
+    const handleCurrencySelection = ({
+        switchUrl, cartCurrencySwitchUrl, currencyCode, warningText, $trigger,
+    }) => {
+        if (!currencyCode) {
+            redirectTo(switchUrl || cartCurrencySwitchUrl);
+            return;
+        }
+
+        if (!cartCurrencySwitchUrl) {
+            redirectTo(switchUrl);
+            return;
+        }
+
+        if (!hasCartId) {
+            redirectTo(switchUrl || cartCurrencySwitchUrl);
+            return;
+        }
+
+        utils.api.cart.getCart({ cartId }, (err, response) => {
+            if (err || !response) {
+                redirectTo(switchUrl || cartCurrencySwitchUrl);
+                return;
+            }
+
+            const hasDiscounts = response.discounts.some(discount => discount.discountedAmount > 0)
                 || response.coupons.length > 0
                 || response.lineItems.giftCertificates.length > 0;
 
-            if (showWarning) {
-                const text = $(event.target).data('warning');
-                const $preModalFocusedEl = $('.navUser-action--currencySelector');
-
-                showAlertModal(text, {
+            if (hasDiscounts && warningText) {
+                showAlertModal(warningText, {
                     icon: 'warning',
                     showCancelButton: true,
-                    $preModalFocusedEl,
+                    $preModalFocusedEl: $trigger,
                     onConfirm: () => {
-                        changeCurrency($(event.target).data('cart-currency-switch-url'), $(event.target).data('currency-code'));
+                        changeCurrency(cartCurrencySwitchUrl, currencyCode, switchUrl);
                     },
                 });
             } else {
-                changeCurrency($(event.target).data('cart-currency-switch-url'), $(event.target).data('currency-code'));
+                changeCurrency(cartCurrencySwitchUrl, currencyCode, switchUrl);
             }
+        });
+    };
+
+    $(document).on('change', '[data-currency-selector-input], #currencySelector_select', event => {
+        const $select = $(event.currentTarget);
+        const $selectedOption = $select.find(':selected');
+
+        const selection = {
+            switchUrl: $selectedOption.val(),
+            cartCurrencySwitchUrl: $selectedOption.data('cartCurrencySwitchUrl'),
+            currencyCode: $selectedOption.data('currencyCode'),
+            warningText: $selectedOption.data('warning'),
+            $trigger: $select,
+        };
+
+        if (!selection.switchUrl && !selection.cartCurrencySwitchUrl) {
+            return;
+        }
+
+        handleCurrencySelection(selection);
+    });
+
+    $(document).on('click', '[data-cart-currency-switch-url]', event => {
+        const $target = $(event.currentTarget);
+        const cartCurrencySwitchUrl = $target.data('cartCurrencySwitchUrl');
+
+        if (!cartCurrencySwitchUrl) {
+            return;
+        }
+
+        event.preventDefault();
+
+        handleCurrencySelection({
+            switchUrl: $target.attr('href'),
+            cartCurrencySwitchUrl,
+            currencyCode: $target.data('currencyCode'),
+            warningText: $target.data('warning'),
+            $trigger: $target,
         });
     });
 }
