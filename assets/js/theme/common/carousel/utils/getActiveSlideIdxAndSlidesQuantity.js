647801bb91a64e9d4058dbdb39f935270bd31f72
Bones <ben@bones.dev>
2026-06-10 15:41:08 +0100

    fix(carousel): improve slide handling and default values in getActiveSlideIdxAndSlidesQuantity

--- assets/js/theme/common/carousel/utils/getActiveSlideIdxAndSlidesQuantity.js
+++ assets/js/theme/common/carousel/utils/getActiveSlideIdxAndSlidesQuantity.js
@@ -1,23 +1,36 @@
-export default ({ slideCount, $slides, options: { slidesToShow, slidesToScroll } }) => {
-    const lastVisibleIdx = $slides.get().reduce((acc, curr, idx) => {
-        if ($(curr).hasClass('slick-active')) return idx;
-        return acc;
-    }, -1);
+export default ({ slideCount = 0, $slides, options = {} } = {}) => {
+  const {
+    slidesToShow: rawSlidesToShow = 1,
+    slidesToScroll: rawSlidesToScroll = 1,
+  } = options;
+  const slidesToShow = Math.max(1, Number(rawSlidesToShow) || 1);
+  const slidesToScroll = Math.max(1, Number(rawSlidesToScroll) || 1);
+  const normalizedSlideCount = Number(slideCount) || 0;
+  const slideElements =
+    $slides && typeof $slides.get === "function" ? $slides.get() : [];
 
-    const activeSlideIdx = lastVisibleIdx < slidesToShow
-        ? 0
-        : Math.ceil((lastVisibleIdx + 1 - slidesToShow) / slidesToScroll);
+  const lastVisibleIdx = slideElements.reduce((acc, curr, idx) => {
+    if ($(curr).hasClass("slick-active")) return idx;
+    return acc;
+  }, -1);
 
-    let slidesQuantity;
-    if (slideCount === 0) {
-        slidesQuantity = 0;
-    } else if (slideCount <= slidesToShow) {
-        slidesQuantity = 1;
-    } else slidesQuantity = Math.ceil((slideCount - slidesToShow) / slidesToScroll) + 1;
+  const activeSlideIdx =
+    lastVisibleIdx < slidesToShow
+      ? 0
+      : Math.ceil((lastVisibleIdx + 1 - slidesToShow) / slidesToScroll);
 
-    // FYI - one slide can contain several card items for product carousel
-    return {
-        activeSlideIdx,
-        slidesQuantity,
-    };
+  let slidesQuantity;
+  if (normalizedSlideCount === 0) {
+    slidesQuantity = 0;
+  } else if (normalizedSlideCount <= slidesToShow) {
+    slidesQuantity = 1;
+  } else
+    slidesQuantity =
+      Math.ceil((normalizedSlideCount - slidesToShow) / slidesToScroll) + 1;
+
+  // FYI - one slide can contain several card items for product carousel
+  return {
+    activeSlideIdx,
+    slidesQuantity,
+  };
 };
